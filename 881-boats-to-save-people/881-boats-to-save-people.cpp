9609f378db54279e17cc78c261583aded04ced24
chinghsuanwei <chinghsuanwei@gmail.com>
2022-03-24 13:17:16 +0800

    Time: 98 ms (67.42%), Space: 42 MB (10.43%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.cpp
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -2,57 +2,20 @@ class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
         
+        sort(people.begin(), people.end());
         
-        
-        map<int, int> dict;
-        
-        for(auto w : people)
-        {
-            if(dict.find(w) == dict.end())
-            {
-                dict[w] = 1;
-            }
-            else
-            {
-                dict[w]++;
-            }              
-        }
-        
-        //find heaviest one and try to find the other person whos weight is smaller than and closest to remained weight
+        int i = 0;
+        int j = people.size() - 1;
         
         int boats = 0;
-        
-        while(dict.size() > 0)
+        while(i <= j)
         {
+            int remain = limit - people[j--];
+            if(remain >= people[i]) i++;
+            
             boats++;
-            int w = std::prev(dict.end())->first;
-            
-            //cout << "Try to remove " << w << endl;
-            dict[w]--;
-            if(dict[w] == 0) dict.erase(w);
-            
-            //try to find if someone fit?
-            
-            if(dict.size() == 0) break;
-            
-            map<int, int>::iterator it = dict.upper_bound(limit-w);
-            
-            if(it != dict.begin())
-            {
-                it--;    
-                //cout << "found suitable " << it->first << endl;
-                dict[it->first]--;
-                if(dict[it->first] == 0)
-                {
-                    dict.erase(it);
-                }
-            }
-            
-            
         }
         
-        
         return boats;
-        
     }
 };
