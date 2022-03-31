b1d300898b636fd710093e10b1e4e199a0183cbf
chinghsuanwei <chinghsuanwei@gmail.com>
2022-03-31 23:01:41 +0800

    Time: 50 ms (66.67%), Space: 14.1 MB (37.77%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.py
+++ 410-split-array-largest-sum/410-split-array-largest-sum.py
@@ -1,48 +1,48 @@
 class Solution:
     
     
-                
+    def canSplit(self, nums: List[int], m: int, limit: int) -> bool:
+        
+        partition, size = 0, 0
+
+        #1 2 3 4 5
+        #take 5
+        # 12 3 4 5
+        # 12 3 
+
+        #take 11
+        # 1234
+
+        # take 9
+        # 123 4 5
+
+        # take 8
+        # 123 4 5
+
+        for i,val in enumerate(nums):
+            if val > limit:
+                return False
+
+            partition += val #45 -> 9
+
+            if partition > limit: # 9 > 8
+                size += 1 # 1
+                #print(size, partition)
+                partition = val # 5
+
+
+                if size == m:
+                    return False
+
+
+        return True        
                 
             
     
     def splitArray(self, nums: List[int], m: int) -> int:
         #binary search
         
-        def canSplit(nums: List[int], m: int, limit: int) -> bool:
         
-            partition, size = 0, 0
-
-            #1 2 3 4 5
-            #take 5
-            # 12 3 4 5
-            # 12 3 
-
-            #take 11
-            # 1234
-
-            # take 9
-            # 123 4 5
-
-            # take 8
-            # 123 4 5
-
-            for i,val in enumerate(nums):
-                if val > limit:
-                    return False
-
-                partition += val #45 -> 9
-
-                if partition > limit: # 9 > 8
-                    size += 1 # 1
-                    #print(size, partition)
-                    partition = val # 5
-                    
-
-                    if size == m:
-                        return False
-
-            
-            return True
         
         total = sum(nums)
         
@@ -55,7 +55,7 @@ class Solution:
             mid = lo + int((hi-lo)/2)
             
             #print("Try mid=", mid)
-            if canSplit(nums, m, mid):
+            if self.canSplit(nums, m, mid):
                 hi = mid #11
             else:
                 lo = mid + 1
