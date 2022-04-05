b85e4d531fc2899c2bc244695a43237411836349
chinghsuanwei <chinghsuanwei@gmail.com>
2022-04-05 11:58:56 +0800

    Time: 39 ms (85.58%), Space: 14.1 MB (92.68%) - LeetHub

--- 1087-brace-expansion/1087-brace-expansion.py
+++ 1087-brace-expansion/1087-brace-expansion.py
@@ -1,40 +1,9 @@
 class Solution:
     def expand(self, s: str) -> List[str]:
         
-        left_brace = -1
         
-        l = []
-        
-        for i, c in enumerate(s):
+        A = s.replace('{', ' ').replace('}', ' ').strip().split( );
+        B = [ sorted(a.split(',')) for a in A ]
+        C = [ ''.join(c) for c in itertools.product(*B)]
             
-            if c == '{':
-                left_brace = i+1
-            elif c == '}':
-                #substr
-                
-                l.append( s[left_brace : i].split(",") )
-                
-                l[len(l)-1].sort()
-                
-                left_brace = -1
-            else:
-                
-                if left_brace == -1:
-                    l.append( list(c) )
-        
-        
-        res = l[0]
-        
-        for i in range( 1, len(l) ):
-            
-            ans = []
-            for str1 in res:
-                for str2 in l[i]:
-                
-                    #print(str1)
-                    #print(str2)
-                    ans.append( str1 + str2 )
-                    
-            res = ans
-            
-        return res
+        return C
