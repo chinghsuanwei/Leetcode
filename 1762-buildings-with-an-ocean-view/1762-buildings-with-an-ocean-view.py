3e62c989f504ac6b04632a39ffef0e078f78974b
chinghsuanwei <chinghsuanwei@gmail.com>
2022-04-14 14:51:20 +0800

    Time: 662 ms (99.37%), Space: 31.5 MB (46.80%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
@@ -3,16 +3,14 @@ class Solution:
         #reversely
         
         n = len(heights)
-        max_h = heights[ n-1 ]
-        q = [n-1]
+        res = [n-1]
         
         for i in range(n-2, -1, -1):
-            h = heights[i]
-            if h > max_h:
-                q.append(i)
-            max_h = max(max_h, h)
+            
+            if heights[i] > heights[ res[-1] ]:
+                res.append(i)
         
-        q.reverse()
-        return q
+        res.reverse()
+        return res
             
             
