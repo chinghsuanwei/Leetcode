089d3109b8e48fa034b8dde1b2c17c1b8178aafa
chinghsuanwei <chinghsuanwei@gmail.com>
2022-04-14 21:16:58 +0800

    Time: 513 ms (99.71%), Space: 14.5 MB (57.68%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -15,14 +15,14 @@ class Solution:
         e_q = []
         
         q.append( (0, 0) )
-        #q.append( (n-1, n-1) )
+        e_q.append( (n-1, n-1) )
         
         dp[0][0] = 1
-        #dp[-1][-1] = -1
+        dp[-1][-1] = -1
         
         step = 2
         direction = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-        while len(q) > 0:
+        while len(q) > 0 and len(e_q) > 0:
             
             # 8 direction
             
@@ -38,18 +38,38 @@ class Solution:
                     new_r = r + br
                     new_c = c + bc
                     
-                    if new_r == n-1 and new_c == n-1:
-                        return step
-                    
-                    
-                        
                     if checkLegalPosition(new_r, new_c) or dp[new_r][new_c] > 0:
                         continue;
+                        
+                    if dp[new_r][new_c] < 0:
+                        return step + abs(dp[new_r][new_c]) - 1
                     
                     q.append( (new_r, new_c) )
                     
                     dp[new_r][new_c] = step
+            
+            size = len(e_q)
+            
+            for i in range(size):
+                r, c = e_q.pop(0)
+                
+                def checkLegalPosition(row, col) -> bool:
+                        return row < 0 or row >= n or col < 0 or col >= n or grid[row][col] == 1
+                
+                for br, bc in direction:
+                    new_r = r + br
+                    new_c = c + bc
                     
+                    if checkLegalPosition(new_r, new_c) or dp[new_r][new_c] < 0:
+                        continue;
+                        
+                    if dp[new_r][new_c] > 0:
+                        return step + abs(dp[new_r][new_c]) - 1
+                    
+                    e_q.append( (new_r, new_c) )
+                    
+                    dp[new_r][new_c] = -step
+            
             step += 1
             
         return -1
